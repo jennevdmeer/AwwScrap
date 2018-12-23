10b1a5b9f3b4c30bba9caf3f59a9c55e993b5fca
Thraxus <mornadak@hotmail.com>
2018-12-23 11:40:32 -0500

    Added defs for all new scrap items.  Just need to wire them in now.

--- AwwScrap/AwwScrap/Data/Scripts/Thraxus/AwwScrapSession.cs
+++ AwwScrap/AwwScrap/Data/Scripts/Thraxus/AwwScrapSession.cs
@@ -4,58 +4,81 @@ using Sandbox.Definitions;
 using Sandbox.ModAPI;
 using VRage.Game;
 using VRage.Game.Components;
+using VRage.Utils;
 
 namespace AwwScrap
 {
-    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
-    // ReSharper disable once ClassNeverInstantiated.Global
-    public class AwwScrapSession : MySessionComponentBase
-    {
-        public static bool LogSetupComplete;
-        public static Log GeneralLog;
-        
-        public override void BeforeStart()
-        {
-            base.BeforeStart();
-            Initialize();
-        }
+	[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+	// ReSharper disable once ClassNeverInstantiated.Global
+	public class AwwScrapSession : MySessionComponentBase
+	{
+		public static bool LogSetupComplete;
+		public static Log GeneralLog;
+		
+		public override void BeforeStart()
+		{
+			base.BeforeStart();
+			Initialize();
+		}
 
-        protected override void UnloadData()
-        {
-            CloseLogs();
-        }
+		protected override void UnloadData()
+		{
+			CloseLogs();
+		}
 
-        private static void InitLogs()
-        {
-            if (Constants.EnableGeneralLog) GeneralLog = new Log(Constants.GeneralLogName);
-            LogSetupComplete = true;
-        }
+		private static void InitLogs()
+		{
+			if (Constants.EnableGeneralLog) GeneralLog = new Log(Constants.GeneralLogName);
+			LogSetupComplete = true;
+		}
 
-        private static void CloseLogs()
-        {
-            if (Constants.EnableGeneralLog) GeneralLog.Close();
-        }
+		private static void CloseLogs()
+		{
+			if (Constants.EnableGeneralLog) GeneralLog.Close();
+		}
 
 
-        private static void Initialize()
-        {
-            InitLogs();
-            MyAPIGateway.Parallel.StartBackground(ScrubCubes);
-        }
+		private static void Initialize()
+		{
+			InitLogs();
+			MyAPIGateway.Parallel.StartBackground(ScrubCubes);
+		}
 
-        private static void ScrubCubes()
-        {
-            MyPhysicalItemDefinition scrapDef = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Scrap"));
-            foreach (MyDefinitionBase myDefinitionBase in MyDefinitionManager.Static.GetAllDefinitions())
-            {
-                MyCubeBlockDefinition myCubeBlockDefinition = myDefinitionBase as MyCubeBlockDefinition;
-                if (myCubeBlockDefinition?.Components == null) continue;
-                GeneralLog.WriteToLog("ScrubCubes",$"{myCubeBlockDefinition.Id}"); 
-                foreach (MyCubeBlockDefinition.Component component in myCubeBlockDefinition.Components)
-                {
-                    component.DeconstructItem = scrapDef;
-                }
-            }
-        }
-    }
+		private static void ScrubCubes()
+		{
+			MyPhysicalItemDefinition scrapDef = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Scrap"));
+			MyPhysicalItemDefinition smallSteelTubeScrap = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "SmallSteelTubeScrap"));
+			MyPhysicalItemDefinition thrusterScrap = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "ThrustScrap"));
+			foreach (MyDefinitionBase myDefinitionBase in MyDefinitionManager.Static.GetAllDefinitions())
+			{
+				MyCubeBlockDefinition myCubeBlockDefinition = myDefinitionBase as MyCubeBlockDefinition;
+				if (myCubeBlockDefinition?.Components == null) continue;
+				GeneralLog.WriteToLog("ScrubCubes",$"{myCubeBlockDefinition.Id}"); 
+				foreach (MyCubeBlockDefinition.Component component in myCubeBlockDefinition.Components)
+				{
+					GeneralLog.WriteToLog("ScrubCubes", $"{component.Definition.Id.SubtypeId}");
+					if (!component.Definition.Public)
+						continue;
+					if (component.Definition.Context == MyModContext.BaseGame)
+					{
+						component.DeconstructItem = scrapDef;
+						continue;
+					}
+					if (component.Definition.Id.SubtypeId == MyStringHash.GetOrCompute("SmallTube"))
+					{
+						GeneralLog.WriteToLog("ScrubCubes", $"Replaced Small Steel Tube");
+						component.DeconstructItem = smallSteelTubeScrap;
+						continue;
+					}
+					if (component.Definition.Id.SubtypeId == MyStringHash.GetOrCompute("Thrust"))
+					{
+						GeneralLog.WriteToLog("ScrubCubes", $"Replaced Thrusters");
+						component.DeconstructItem = thrusterScrap;
+						continue;
+					}
+					component.DeconstructItem = scrapDef;
+				}
+			}
+		}
+	}
 }
---
+++ AwwScrap/AwwScrap/Data/Scripts/Thraxus/Helpers/Definitions.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Definitions;
+
+namespace AwwScrap.AwwScrap.Data.Scripts.Thraxus.Helpers
+{
+	class Definitions
+	{
+		/* Original Intent (Model textures based on original idea, so recorded for later use)
+		 * Scrap 1: 3.0 Iron, 1.25 Nickle, 0.75 Cobalt
+		 * Scrap 2: 3.75 Silicon
+		 * Scrap 3: 3.75 Iron, 5.0 Stone, 1.25 Silver
+		 * Scrap 4: 7.5 Iron, 2.5 Cobalt, 0.25 Gold, 0.1 Platinum
+		 * Scrap 5: 1.25 Silver, 2.50 Gold, 55.0 Cobalt, 150.0 Iron
+		 * Scrap 6: 15.0 Iron, 17.5 Nickel, 5.0 Silver
+		 * Scrap 7: 1.25 Iron, 2.5 Nickel
+		 * Scrap 8: 0.2 Silicon, 1.0 Iron
+		 * Scrap 9: 0.13 Silicon, 0.5 Magnesium
+		 * Scrap 10: 2.5 Nickel, 2.0 Silicon
+		 * Scrap 11: 2.5 Iron, 0.25 Silicon, 0.5 Nickel
+		 * Scrap 12: No custom model; reuse standard scrap, name: Iron Scrap -- 5.25 Iron
+		 * Scrap Default: Anything that doesn't fit the above criteria
+		 * 
+		 * 
+		 * Modified Intent (Each component will have dedicated scrap @ 0.75 original creation value)
+		 *
+		 * Bulletproof Glass: 
+		 *		5.0 Silicon 
+		 *		Scrap 2 @ 3.75 Silicon
+		 *
+		 * Computer: 
+		 *		0.17 Iron, 0.07 Silicon 
+		 *		Scrap 8 @ 0.1275 Iron, 0.0525 Silicon
+		 *
+		 * Construction Component: 
+		 *		3.33 Iron 
+		 *		Scrap 12 @ 2.4975 Iron
+		 *
+		 * Detector Component: 
+		 *		1.67 Iron, 5.0 Nickel 
+		 *		Scrap 7 @ 1.2525 Iron, 3.75 Nickel
+		 *
+		 * Display: 
+		 *		0.33 Iron, 1.67 Silicon
+		 *		Scrap 8 @ 0.2475 Iron, 1.2525 Silicon
+		 *
+		 * Girder: 
+		 *		2.33 Iron
+		 *		Scrap 12 @ 1.7475 Iron
+		 *
+		 * Gravity Component: 
+		 *		1.67 Silver, 3.33 Gold, 73.33 Cobalt, 200.0 Iron
+		 *		Scrap 5 @ 1.2525 Silver, 2.4975 Gold, 54.9975 Cobalt, 150.0 Iron
+		 *
+		 * Interior Plate: 
+		 *		1.17 Iron 
+		 *		Scrap 12 @ 0.8875 Iron
+		 *
+		 * Large Steel Tube: 
+		 *		10.0 Iron 
+		 *		Scrap 12 @ 7.5 Iron
+		 *
+		 * Medical Component: 
+		 *		20.0 Iron, 23.33 Nickel, 6.67 Silver
+		 *		Scrap 6 @ 15.0 Iron, 17.4975 Nickel, 5.0025 Silver
+		 *
+		 * Metal Grid: 
+		 *		4.0 Iron, 1.67 Nickel, 1.0 Cobalt
+		 *		Scrap 1 @ 3.0 Iron, 1.2525 Nickel, 0.75 Cobalt
+		 *
+		 * Motor: 
+		 *		6.67 Iron, 1.67 Nickel 
+		 *		Scrap 7 @ 5.0025 Iron, 1.2525 Nickel
+		 *
+		 * Power Cell: 
+		 *		3.33 Iron, 0.33 Silicon, 0.67 Nickel 
+		 *		Scrap 11 @ 2.4975 Iron, 0.2475 Silicon, 0.5025 Nickel
+		 *
+		 * Radio-comm Component: 
+		 *		2.67 Iron, 0.33 Silicon 
+		 *		Scrap 8 @ 2.0025 Iron, 0.2475 Silicon
+		 *
+		 * Reactor Component: 
+		 *		5.0 Iron, 6.67 Gravel, 1.67 Silver
+		 *		Scrap 3 @ 3.75 Iron, 5.0025 Gravel, 1.2525 Silver
+		 *
+		 * Small Steel Tube: 
+		 *		1.67 Iron 
+		 *		Scrap 12 @ 1.2525 Iron
+		 *
+		 * Solar Cell: 
+		 *		3.33 Nickel, 2.67 Silicon 
+		 *		Scrap 10 @ 2.4975 Nickel, 2.0025 Silicon
+		 *
+		 * Steel Plate: 
+		 *		7.0 Iron 
+		 *		Scrap 12 @ 5.25 Iron
+		 *
+		 * Superconductor: 
+		 *		3.33 Iron, 0.67 Gold 
+		 *		Scrap 4 @ 2.4975 Iron, 0.5025 Gold
+		 *
+		 * Thruster Component: 
+		 *		10 Iron, 3.33 Cobalt, 0.33 Gold, 0.13 Platinum 
+		 *		Scrap 4 @ 7.5 Iron, 2.4975 Cobalt, 0.2475 Gold, 0.0975 Platinum
+		 */
+	}
+}

3d362732adeb56e8e7e62aecd120a2ee07ee6ed8
Thraxus <mornadak@hotmail.com>
2019-11-09 22:27:15 -0500

    Added compatibility for EWE and Defense Shields.  Also refactored a lot of the minimal code.  Nedxt update will be merging this with my mod template and adding user configs.

--- AwwScrap/AwwScrap/Data/Scripts/Thraxus/AwwScrapSession.cs
+++ AwwScrap/AwwScrap/Data/Scripts/Thraxus/AwwScrapSession.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using AwwScrap.Helpers;
 using AwwScrap.Utilities;
 using Sandbox.Definitions;
 using Sandbox.ModAPI;
+using VRage;
 using VRage.Game;
 using VRage.Game.Components;
 using VRage.Utils;
@@ -45,7 +47,7 @@ namespace AwwScrap
 			MyAPIGateway.Parallel.StartBackground(ScrubCubes);
 		}
 
-		private static Dictionary<MyStringHash, string> ComponentDictionary = new Dictionary<MyStringHash, string>
+		private static readonly Dictionary<MyStringHash, string> ComponentDictionary = new Dictionary<MyStringHash, string>
 		{
 			{ MyStringHash.GetOrCompute("BulletproofGlass"), "BulletproofGlassScrap" },
 			{ MyStringHash.GetOrCompute("Computer"), "ComputerScrap" },
@@ -67,47 +69,68 @@ namespace AwwScrap
 			{ MyStringHash.GetOrCompute("SolarCell"), "SolarCellScrap" },
 			{ MyStringHash.GetOrCompute("SteelPlate"), "SteelPlateScrap" },
 			{ MyStringHash.GetOrCompute("Superconductor"), "SuperconductorScrap" },
-			{ MyStringHash.GetOrCompute("Thrust"), "ThrustScrap" }
+			{ MyStringHash.GetOrCompute("Thrust"), "ThrustScrap" },
+			{ MyStringHash.GetOrCompute("ShieldComponent"), "FieldEmitterScrap" }
+		};
+
+		private static readonly List<MyStringHash> AwwScrapSubTypeIds = new List<MyStringHash>
+		{
+			MyStringHash.GetOrCompute("BulletproofGlassToIngot"),
+			MyStringHash.GetOrCompute("ComputerToIngot"),
+			MyStringHash.GetOrCompute("ConstructionToIngot"),
+			MyStringHash.GetOrCompute("DetectorToIngot"),
+			MyStringHash.GetOrCompute("DisplayToIngot"),
+			MyStringHash.GetOrCompute("ExplosivesToIngot"),
+			MyStringHash.GetOrCompute("GirderToIngot"),
+			MyStringHash.GetOrCompute("GravityGeneratorToIngot"),
+			MyStringHash.GetOrCompute("InteriorPlateToIngot"),
+			MyStringHash.GetOrCompute("LargeTubeToIngot"),
+			MyStringHash.GetOrCompute("MedicalToIngot"),
+			MyStringHash.GetOrCompute("MetalGridToIngot"),
+			MyStringHash.GetOrCompute("MotorToIngot"),
+			MyStringHash.GetOrCompute("PowerCellToIngot"),
+			MyStringHash.GetOrCompute("RadioCommunicationToIngot"),
+			MyStringHash.GetOrCompute("ReactorToIngot"),
+			MyStringHash.GetOrCompute("SmallTubeToIngot"),
+			MyStringHash.GetOrCompute("SolarCellToIngot"),
+			MyStringHash.GetOrCompute("SteelPlateToIngot"),
+			MyStringHash.GetOrCompute("SuperconductorToIngot"),
+			MyStringHash.GetOrCompute("ThrustToIngot")
 		};
 
 		private static void ScrubCubes()
 		{
-			MyPhysicalItemDefinition scrapDef = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Scrap"));
-			foreach (MyDefinitionBase myDefinitionBase in MyDefinitionManager.Static.GetAllDefinitions())
+			// This loop accounts for World Settings for the Assembler Efficiency Modifier (x1, x3, x10)
+			foreach (MyBlueprintDefinitionBase myBlueprintDefinitionBase in AwwScrapSubTypeIds.Select(
+				subtype => MyDefinitionManager.Static.GetBlueprintDefinition(
+					new MyDefinitionId(typeof(MyObjectBuilder_BlueprintDefinition), subtype))))
 			{
-				MyCubeBlockDefinition myCubeBlockDefinition = myDefinitionBase as MyCubeBlockDefinition;
-				if (myCubeBlockDefinition?.Components == null) continue;
-
-				//GeneralLog.WriteToLog("ScrubCubes",$"{myCubeBlockDefinition.Id}"); 
-
+				for (int index = 0; index < myBlueprintDefinitionBase.Results.Length; index++)
+				{	// MyFixedPoint can't do /= operations, so have to do a work around
+					float f = (float) myBlueprintDefinitionBase.Results[index].Amount;
+					f /= MyAPIGateway.Session.SessionSettings.AssemblerEfficiencyMultiplier;
+					myBlueprintDefinitionBase.Results[index].Amount = (MyFixedPoint) f;
+				}
+			}
+			
+			MyPhysicalItemDefinition scrapDef = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Scrap"));
+			
+			foreach (MyCubeBlockDefinition myCubeBlockDefinition in MyDefinitionManager.Static.GetAllDefinitions().Select(myDefinitionBase => myDefinitionBase as MyCubeBlockDefinition).Where(myCubeBlockDefinition => myCubeBlockDefinition?.Components != null))
+			{
 				foreach (MyCubeBlockDefinition.Component component in myCubeBlockDefinition.Components)
 				{
 					GeneralLog.WriteToLog("ScrubCubes", $"{component.Definition.Id.SubtypeId}");
 
 					if (!component.Definition.Public)
 						continue;
-					
+
 					string subtypeName;
 					if (ComponentDictionary.TryGetValue(component.Definition.Id.SubtypeId, out subtypeName))
 					{
 						component.DeconstructItem = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), subtypeName));
 						continue;
 					}
-
-					component.DeconstructItem = scrapDef; 
-					//if (component.Definition.Id.SubtypeId == MyStringHash.GetOrCompute("SmallTube"))
-					//{
-					//	GeneralLog.WriteToLog("ScrubCubes", $"Replaced Small Steel Tube");
-					//	component.DeconstructItem = smallSteelTubeScrap;
-					//	continue;
-					//}
-					//if (component.Definition.Id.SubtypeId == MyStringHash.GetOrCompute("Thrust"))
-					//{
-					//	GeneralLog.WriteToLog("ScrubCubes", $"Replaced Thrusters");
-					//	component.DeconstructItem = thrusterScrap;
-					//	continue;
-					//}
-					//component.DeconstructItem = scrapDef;
+					component.DeconstructItem = scrapDef;
 				}
 			}
 		}
